73bfa85260cb27eb0b3b12e6cf67579d52db7fe6
TitusKI <kidumazie@gmail.com>
2025-05-07 22:23:57 +0300

    Implement ServiceRepository with CRUD operations and error handling

---
+++ lib/data/repositories/service_repository_impl.dart
@@ -0,0 +1,117 @@
+import 'package:dartz/dartz.dart';
+import 'package:service_booking/data/datasources/service_local_ds.dart';
+import 'package:service_booking/data/datasources/service_remote_ds.dart';
+import 'package:service_booking/data/model/service_model_dto.dart';
+import '../../core/errors/exceptions.dart';
+import '../../core/errors/failures.dart';
+import '../../domain/entities/service_entity.dart';
+import '../../domain/repositories/service_repository.dart';
+
+class ServiceRepositoryImpl implements ServiceRepository {
+  final ServiceRemoteDataSource remoteDataSource;
+  final ServiceLocalDataSource localDataSource;
+
+  ServiceRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+  });
+
+  @override
+  Future<Either<Failure, List<ServiceEntity>>> getAllServices() async {
+    try {
+      final remoteServices = await remoteDataSource.getAllServices();
+      localDataSource.cacheServices(remoteServices);
+      print('Services are ${remoteServices.length}');
+      print('Services are $remoteServices');
+
+      return Right(remoteServices);
+    } on ServerException {
+      try {
+        final localServices = await localDataSource.getLastServices();
+        return Right(localServices);
+      } on CacheException {
+        return Left(
+          ServerFailure(
+            message: 'Failed to fetch services and no cache available.',
+          ),
+        );
+      }
+    } on CacheException {
+      return Left(
+        CacheFailure(message: 'Error during initial data fetching or caching.'),
+      );
+    } catch (e) {
+      return Left(
+        ServerFailure(message: 'An unexpected error occurred during fetch.'),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, ServiceEntity>> getServiceDetail(String id) async {
+    try {
+      final remoteService = await remoteDataSource.getServiceDetail(id);
+      return Right(remoteService);
+    } on ServerException {
+      return Left(ServerFailure(message: 'Failed to get service detail'));
+    } on CacheException {
+      return Left(CacheFailure());
+    } catch (e) {
+      return Left(
+        ServerFailure(
+          message: 'An unexpected error occurred while getting detail',
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, ServiceEntity>> addService(
+    ServiceEntity service,
+  ) async {
+    try {
+      final serviceDto = ServiceModelDto.fromEntity(service);
+      final newService = await remoteDataSource.addService(serviceDto);
+      return Right(newService);
+    } on ServerException {
+      return Left(ServerFailure(message: 'Failed to add service'));
+    } catch (e) {
+      return Left(
+        ServerFailure(message: 'An unexpected error occurred while adding'),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, ServiceEntity>> updateService(
+    ServiceEntity service,
+  ) async {
+    try {
+      final serviceDto = ServiceModelDto.fromEntity(service);
+      final updatedService = await remoteDataSource.updateService(serviceDto);
+      return Right(updatedService);
+    } on ServerException {
+      return Left(ServerFailure(message: 'Failed to update service'));
+    } on CacheException {
+      return Left(CacheFailure());
+    } catch (e) {
+      return Left(
+        ServerFailure(message: 'An unexpected error occurred while updating'),
+      );
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> deleteService(String id) async {
+    try {
+      await remoteDataSource.deleteService(id);
+      return const Right(null);
+    } on ServerException {
+      return Left(ServerFailure(message: 'Failed to delete service'));
+    } catch (e) {
+      return Left(
+        ServerFailure(message: 'An unexpected error occurred while deleting'),
+      );
+    }
+  }
+}
